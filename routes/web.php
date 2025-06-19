09d0ea4a7893bdf027c34d95e4ad8b7254f7942c
Jose Hernandez <hernandezmartinezjose2017@gmail.com>
2025-06-19 11:40:12 -0600

    mejora de login

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,14 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-<<<<<<< HEAD
-=======
 use App\Http\Controllers\Admin\UserController;
 use App\Http\Controllers\ReferenciaController;
->>>>>>> recuperar-trabajo
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/dashboard', function () {
-<<<<<<< HEAD
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-=======
     if (Auth::user()->hasRole('SuperAdmin')) {
         return view('dashboard_admin');
     } else {
@@ -24,17 +17,12 @@ Route::get('/dashboard', function () {
     }
 })->middleware('auth')->name('dashboard');
 
->>>>>>> recuperar-trabajo
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
-<<<<<<< HEAD
-
-require __DIR__.'/auth.php';
-=======
 Route::middleware(['auth', 'SuperAdminOnly'])->prefix('admin')->name('admin.')->group(function () {
     Route::resource('users', UserController::class);
 });
@@ -43,4 +31,3 @@ Route::middleware(['auth'])->group(function () {
     Route::resource('referencias', ReferenciaController::class)->only(['index', 'create', 'store', 'edit', 'update']);
 });
 require __DIR__ . '/auth.php';
->>>>>>> recuperar-trabajo
